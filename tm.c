1a4fec7e38cede267f0c0a9ba15b6b49d80d9e7f
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-12-19 14:01:44 +0530

    Update tm.c

--- tm.c
+++ tm.c
@@ -5,10 +5,7 @@
 #include <linux/sched.h>
 #include <asm/uaccess.h>
 
-extern void *sys_call_table[];
-
-static int uid;
-module_param(uid, int, 0644);
+unsigned long **sys_call_table;
 asmlinkage int (*original_call) (const char *, int, int);
 asmlinkage int task_module(pid_t pid) {
     struct task_struct *task;
@@ -22,20 +19,32 @@ asmlinkage int task_module(pid_t pid) {
     return original_call(source, destination);
 }
 
+static unsigned long **aquire_sys_call_table(void)
+{
+    unsigned long int offset = PAGE_OFFSET;
+    unsigned long **sct;
+    while (offset < ULLONG_MAX) {
+        sct = (unsigned long **)offset;
+        if (sct[__NR_close] == (unsigned long *) sys_close)
+            return sct;
+        offset += sizeof(void *);
+    }
+    return NULL;
+}
+
 static int __init tm_init() {
-    original_call = sys_call_table[__NR_kernel_2D_memcpy];
-    sys_call_table[__NR_kernel_2D_memcpy] = task_module;
-    printk(KERN_INFO "Spying on UID:%d\n", uid);
+     if(!(sys_call_table = aquire_sys_call_table()))
+        return -1;
+    original_call = sys_call_table[__NR_getpgid];
+    sys_call_table[__NR_getpgid] = task_module;
     return 0;
 }
-
 static void __exit tm_exit() {
-    if (sys_call_table[__NR_kernel_2D_memcpy] != task_module) {
+    if (sys_call_table[__NR_getpgid] != task_module) {
         printk(KERN_ALERT "Bruh Moment");
     }
-    sys_call_table[__NR_kernel_2D_memcpy] = original_call;
+    sys_call_table[__NR_getpgid] = original_call;
 }
-
 module_init(tm_init);
 module_exit(tm_exit);
 MODULE_LICENSE("GPL");
