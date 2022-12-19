b67811d2953299bd5553193cfae4f694504afd21
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-12-20 01:46:01 +0530

    Create A1.c

---
+++ A1.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int forks[5];
+
+void free_fork(int *frok, int x) {
+    frok[x] = 0;
+}
+
+void take_fork(int *frok, int x) {
+    frok[x] = 1;
+}
+
+void *philosopher1() {
+    while (true) {
+        printf("Philosopher 1 is thinking\n");
+        sleep(1);
+        int left = 0;
+        int right = 1;
+        while (forks[right] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, right);
+        printf("Philosopher 1 picked right fork\n");
+        while (forks[left] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, left);
+        printf("Philosopher 1 picked left fork\n");
+        printf("Philosopher 1 is eating\n");
+        sleep(1);
+        free_fork(forks, left);
+        printf("Philosopher 1 placed left fork back\n");
+        free_fork(forks, right);
+        printf("Philosopher 1 placed right fork back\n");
+    }
+}
+void *philosopher2() {
+    while (true) {
+        printf("Philosopher 2 is thinking\n");
+        sleep(1);
+        int left = 1;
+        int right = 2;
+        while (forks[left] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, left);
+        printf("Philosopher 2 picked left fork\n");
+        while (forks[right] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, right);
+        printf("Philosopher 2 picked right fork\n");
+        printf("Philosopher 2 is eating\n");
+        sleep(1);
+        free_fork(forks, left);
+        printf("Philosopher 2 placed left fork back\n");
+        free_fork(forks, right);
+        printf("Philosopher 2 placed right fork back\n");
+    }
+}
+void *philosopher3() {
+    while (true) {
+        printf("Philosopher 3 is thinking\n");
+        sleep(1);
+        int left = 2;
+        int right = 3;
+        while (forks[right] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, right);
+        printf("Philosopher 3 picked right fork\n");
+        while (forks[left] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, left);
+        printf("Philosopher 3 picked left fork\n");
+        printf("Philosopher 3 is eating\n");
+        sleep(1);
+        free_fork(forks, left);
+        printf("Philosopher 3 placed left fork back\n");
+        free_fork(forks, right);
+        printf("Philosopher 3 placed right fork back\n");
+    }
+}
+void *philosopher4() {
+    while (true) {
+        printf("Philosopher 4 is thinking\n");
+        sleep(1);
+        int left = 3;
+        int right = 4;
+        while (forks[left] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, left);
+        printf("Philosopher 4 picked left fork\n");
+        while (forks[right] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, right);
+        printf("Philosopher 4 picked right fork\n");
+        printf("Philosopher 4 is eating\n");
+        sleep(1);
+        free_fork(forks, left);
+        printf("Philosopher 4 placed left fork back\n");
+        free_fork(forks, right);
+        printf("Philosopher 4 placed right fork back\n");
+    }
+}
+void *philosopher5() {
+    while (true) {
+        printf("Philosopher 5 is thinking\n");
+        sleep(1);
+        int left = 4;
+        int right = 0;
+        while (forks[right] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, right);
+        printf("Philosopher 5 picked right fork\n");
+        while (forks[left] == 1) {
+            sleep(1);
+        }
+        take_fork(forks, left);
+        printf("Philosopher 5 picked left fork\n");
+        printf("Philosopher 5 is eating\n");
+        sleep(1);
+        free_fork(forks, left);
+        printf("Philosopher 5 placed left fork back\n");
+        free_fork(forks, right);
+        printf("Philosopher 5 placed right fork back\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < 5; i++) {
+        forks[i] = 0;
+    }
+    pthread_t p1, p2, p3, p4, p5;
+    pthread_create(&p1, NULL, philosopher1, NULL);
+    pthread_create(&p2, NULL, philosopher2, NULL);
+    pthread_create(&p3, NULL, philosopher3, NULL);
+    pthread_create(&p4, NULL, philosopher4, NULL);
+    pthread_create(&p5, NULL, philosopher5, NULL);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+    pthread_join(p3, NULL);
+    pthread_join(p4, NULL);
+    pthread_join(p5, NULL);
+    return 0;
+}
