2701315fe2eef867d2f39a9158d610c52f1e00d5
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-12-20 21:58:01 +0530

    Update P2.c

--- Q2/Shared Memory/P2.c
+++ Q2/Shared Memory/P2.c
@@ -10,28 +10,24 @@
 #include <sys/socket.h>
 #include <sys/shm.h>
 
-void printArraydet(char array[]) {
-    printf("Max Index Received by P1: %d\n",array[5]);
-    printf("String received by P1:");
-    for(int j = 0 ; j < 5; j++){
-        printf("%c",array[j]);
-    }
-    printf("\n");
-}
-
 int main(){
     sleep(2);
     char buff[5][6];
-    int mem = shmget(6969,100,IPC_CREAT | 0666);
+    int mem = shmget(9999,100,IPC_CREAT | 0666);
     char *ptr = (char *)shmat(mem,NULL,0);
     for(int i = 0 ; i < 10 ; i++){
         for(int j = 0 ; j < 5 ; j++){
             strcpy(buff[j],ptr);
             sleep(1);
         }
-        printArraydet(buff[4]);
+        printf("Max index received from P1: %d\n",buff[4][5]);
+        printf("String:");
+        for(int j = 0 ; j < 5; j++){
+            printf("%c",buff[4][j]);
+        }
+        printf("\n");
         strcpy(ptr,buff[4]);
         sleep(1);
     }
-    printf("All strings received \n");
+    printf("All strings received \n");
 }
