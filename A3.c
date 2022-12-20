34bb4fbbb9d21a5fe675685ca84599616d44bc25
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-12-20 18:41:42 +0530

    Update A3.c

--- A3.c
+++ A3.c
@@ -5,26 +5,31 @@
 #include <stdbool.h>
 #include <semaphore.h>
 
-sem_t forks[5];
-sem_t bowls[2];
+sem_t fork1;
+sem_t fork2;
+sem_t fork3;
+sem_t fork4;
+sem_t fork5;
+sem_t bowl1;
+sem_t bowl2;
 
 void *philosopher1() {
     while (true) {
         printf("Philosopher 1 is thinking\n");
         sleep(1);
-        sem_wait(&forks[0]);
+        sem_wait(&fork1);
         printf("Philosopher 1 picked left fork\n");
-        sem_wait(&forks[1]);
+        sem_wait(&fork2);
         printf("Philosopher 1 picked right fork\n");
-        sem_wait(&bowls[0]);
+        sem_wait(&bowl1);
         printf("Philosopher 1 picked a sauce bowl\n");
         printf("Philosopher 1 is eating\n");
         sleep(1);
-        sem_post(&bowls[0]);
+        sem_post(&bowl1);
         printf("Philosopher 1 placed the sauce bowl back\n");
-        sem_post(&forks[0]);
+        sem_post(&fork1);
         printf("Philosopher 1 placed left fork back\n");
-        sem_post(&forks[1]);
+        sem_post(&fork2);
         printf("Philosopher 1 placed right fork back\n");
     }
 }
@@ -32,19 +37,19 @@ void *philosopher2() {
     while (true) {
         printf("Philosopher 2 is thinking\n");
         sleep(1);
-        sem_wait(&forks[2]);
-        printf("Philosopher 2 picked right fork\n");
-        sem_wait(&forks[1]);
-        printf("Philosopher 2 picked left fork\n");
-        sem_wait(&bowls[1]);
+        sem_wait(&fork2);
+        printf("Philosopher 1 picked left fork\n");
+        sem_wait(&fork3);
+        printf("Philosopher 1 picked right fork\n");
+        sem_wait(&bowl2);
         printf("Philosopher 2 picked a sauce bowl\n");
         printf("Philosopher 2 is eating\n");
         sleep(1);
-        sem_post(&bowls[1]);
+        sem_post(&bowl2);
         printf("Philosopher 2 placed the sauce bowl back\n");
-        sem_post(&forks[1]);
+        sem_post(&fork2);
         printf("Philosopher 2 placed left fork back\n");
-        sem_post(&forks[2]);
+        sem_post(&fork3);
         printf("Philosopher 2 placed right fork back\n");
     }
 }
@@ -52,19 +57,19 @@ void *philosopher3() {
     while (true) {
         printf("Philosopher 3 is thinking\n");
         sleep(1);
-        sem_wait(&forks[2]);
+        sem_wait(&fork3);
         printf("Philosopher 3 picked left fork\n");
-        sem_wait(&forks[3]);
+        sem_wait(&fork4);
         printf("Philosopher 3 picked right fork\n");
-        sem_wait(&bowls[0]);
+        sem_wait(&bowl1);
         printf("Philosopher 3 picked a sauce bowl\n");
         printf("Philosopher 3 is eating\n");
         sleep(1);
-        sem_post(&bowls[0]);
+        sem_post(&bowl1);
         printf("Philosopher 3 placed the sauce bowl back\n");
-        sem_post(&forks[2]);
+        sem_post(&fork3);
         printf("Philosopher 3 placed left fork back\n");
-        sem_post(&forks[3]);
+        sem_post(&fork4);
         printf("Philosopher 3 placed right fork back\n");
     }
 }
@@ -72,19 +77,19 @@ void *philosopher4() {
     while (true) {
         printf("Philosopher 4 is thinking\n");
         sleep(1);
-        sem_wait(&forks[4]);
-        printf("Philosopher 4 picked right fork\n");
-        sem_wait(&forks[3]);
-        printf("Philosopher 4 picked left fork\n");
-        sem_wait(&bowls[1]);
+        sem_wait(&fork4);
+        printf("Philosopher 1 picked left fork\n");
+        sem_wait(&fork5);
+        printf("Philosopher 1 picked right fork\n");
+        sem_wait(&bowl2);
         printf("Philosopher 4 picked a sauce bowl\n");
         printf("Philosopher 4 is eating\n");
         sleep(1);
-        sem_post(&bowls[1]);
+        sem_post(&bowl2);
         printf("Philosopher 4 placed the sauce bowl back\n");
-        sem_post(&forks[3]);
+        sem_post(&fork4);
         printf("Philosopher 4 placed left fork back\n");
-        sem_post(&forks[4]);
+        sem_post(&fork5);
         printf("Philosopher 4 placed right fork back\n");
     }
 }
@@ -92,29 +97,31 @@ void *philosopher5() {
     while (true) {
         printf("Philosopher 5 is thinking\n");
         sleep(1);
-        sem_wait(&forks[4]);
+        sem_wait(&forks5);
         printf("Philosopher 5 picked left fork\n");
-        sem_wait(&forks[0]);
+        sem_wait(&fork1);
         printf("Philosopher 5 picked right fork\n");
-        sem_wait(&bowls[0]);
+        sem_wait(&bowl1);
         printf("Philosopher 5 picked a sauce bowl\n");
         printf("Philosopher 5 is eating\n");
         sleep(1);
-        sem_post(&bowls[0]);
+        sem_post(&bowl1);
         printf("Philosopher 5 placed the sauce bowl back\n");
-        sem_post(&forks[4]);
+        sem_post(&fork5);
         printf("Philosopher 5 placed left fork back\n");
-        sem_post(&forks[0]);
+        sem_post(&fork1);
         printf("Philosopher 5 placed right fork back\n");
     }
 }
 
 int main() {
-    for (int i = 0; i < 5; i++) {
-        sem_init(&forks[i], 0, 1);
-    }
-    sem_init(&bowls[0], 0, 1);
-    sem_init(&bowls[1], 0, 1);
+    sem_init(&fork1, 0, 1);
+    sem_init(&fork2, 0, 1);
+    sem_init(&fork3, 0, 1);
+    sem_init(&fork4, 0, 1);
+    sem_init(&fork5, 0, 1);
+    sem_init(&bowl1, 0, 1);
+    sem_init(&bowl2, 0, 1);
     pthread_t p1, p2, p3, p4, p5;
     pthread_create(&p1, NULL, philosopher1, NULL);
     pthread_create(&p2, NULL, philosopher2, NULL);
@@ -126,10 +133,12 @@ int main() {
     pthread_join(p3, NULL);
     pthread_join(p4, NULL);
     pthread_join(p5, NULL);
-    for (int i = 0; i < 5; i++) {
-        sem_destroy(&forks[i]);
-    }
-    sem_destroy(&bowls[0]);
-    sem_destroy(&bowls[1]);
+    sem_destroy(&fork1);
+    sem_destroy(&fork2);
+    sem_destroy(&fork3);
+    sem_destroy(&fork4);
+    sem_destroy(&fork5);
+    sem_destroy(&bowl1);
+    sem_destroy(&bowl2);
     return 0;
 }
