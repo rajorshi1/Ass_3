5ccc50b6467f3008d79a9cf8b3d7c4f0e1ab9600
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-12-21 15:34:59 +0530

    Update P2.c

--- Q2/FIFO/P2.c
+++ Q2/FIFO/P2.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void printCharArray(char buff[5][6]){
+void printCharArray(char toBeSent[5][6]){
     printf("The strings received from P1: \n");
     for (int i = 0; i < 5; i++) {
         for (int j=0; j<5; j++) {
-            printf("%c",buff[i][j]);
+            printf("%c",toBeSent[i][j]);
         }
         printf(" ");
     }
     printf("\n");
-    printf("Max Index received from P1: %d\n", buff[4][5]);
+    printf("Max Index received from P1: %d\n", toBeSent[4][5]);
 }
 
 int main()
@@ -22,12 +23,28 @@ int main()
     char destArr[5][6];
     for (int i=0; i<10; i++) {
         int fd = open("fifoString", O_RDONLY);
-        read(fd, &destArr, sizeof(char)*30);
+        if (!fd) {
+            perror("Couldn't Open\n");
+            exit(1);
+        }
+        int z = read(fd, &destArr, sizeof(char)*30);
+        if (!z) {
+            perror("Couldn't Read\n");
+            exit(1);
+        }
         close(fd);
         printCharArray(destArr);
         int temp = destArr[4][5];
         fd = open("fifoString", O_WRONLY);
-        write(fd, &temp, sizeof(int));
+        if (!fd) {
+            perror("Couldn't Open\n");
+            exit(1);
+        }
+        int y = write(fd, &temp, sizeof(int));
+        if (!y) {
+            perror("Couldn't Write\n");
+            exit(1);
+        }
         close(fd);
     }
     return 0;
